d699779986caaeaa19957ba8cb71d803970fc2ee
Mirianne dos Santos <mirianneds@gmail.com>
2025-11-13 11:36:03 +0100

    update

---
+++ T1/Ejercicio3_variables/src/Ejercicio3_variables.java
@@ -0,0 +1,21 @@
+public class Ejercicio3_variables {
+
+    static void main(String[] args) {
+
+        String nombre = "Mirianne";
+        System.out.println("Nombre: "+nombre);
+
+        int edad = 27;
+        System.out.println("Edad: "+edad);
+
+        boolean estudiante = true;
+        System.out.println("Es estudiante?: "+estudiante);
+
+        double altura = 1.68;
+        System.out.println("Altura: "+altura);
+
+        char inicial = 'M';
+        System.out.println("Inicial: "+inicial);
+
+    }
+}
