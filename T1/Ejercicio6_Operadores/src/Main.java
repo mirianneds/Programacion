51fa7e35a9dad5f4d66d7108c50bee925fef6ebc
Mirianne dos Santos <mirianneds@gmail.com>
2025-11-13 14:49:14 +0100

    update

---
+++ T1/Ejercicio6_Operadores/src/Main.java
@@ -0,0 +1,27 @@
+/*Escribe un programa que declare dos variables numéricas,
+realice las operaciones básicas (suma, resta, multiplicación y división)
+y muestre los resultados por consola.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero1 = 8;
+        int numero2 = 5;
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 - numero2;
+        double division = numero1 / numero2;
+
+        System.out.println("Numero 1: "+numero1);
+        System.out.println("Numero 2: "+numero2);
+        System.out.println("Suma: "+suma);
+        System.out.println("Resta: "+resta);
+        System.out.println("Multilicación: "+multiplicacion);
+        System.out.println("División: "+division);
+
+
+    }
+}
+
