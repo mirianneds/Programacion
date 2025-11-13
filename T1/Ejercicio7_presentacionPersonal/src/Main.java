d508f93e5715fc4d7bdc224dfec889c11ee4ba63
Mirianne dos Santos <mirianneds@gmail.com>
2025-11-13 16:54:46 +0100

    update

---
+++ T1/Ejercicio7_presentacionPersonal/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static void main(String[] args) {
+
+            String nombre = "Elena";
+            String apellido = "Garcia";
+            int edad = 24;
+            String ciudad = "Barcelona";
+            System.out.printf("Hola, me llamo %s %s, tengo %d años y vivo en %s", nombre, apellido,edad,ciudad);
+
+    }
+}
