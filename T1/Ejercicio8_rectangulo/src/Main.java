0599687b61295e8a66cb0876b861d0e45b6b9e48
Mirianne dos Santos <mirianneds@gmail.com>
2025-11-13 17:00:52 +0100

    update

---
+++ T1/Ejercicio8_rectangulo/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static void main(String[] args) {
+        int base = 5;
+        int altura = 3;
+        System.out.printf("""
+                Base del rectangulo: %d
+                Altura del resctangulo: %d
+                El area del rectangulo es: %d
+                """, base,altura,base*altura);
+
+    }
+}
